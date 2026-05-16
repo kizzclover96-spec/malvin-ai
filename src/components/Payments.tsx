629aac43749ec015c8cb1102cbb9d444c16c9fa4
Praise Imasuen <kizzclover96@gmail.com>
2026-05-16 16:31:15 +0200

    mom

--- src/components/Payments.tsx
+++ src/components/Payments.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { auth, db } from '../firebase';
+import { auth, db } from "../firebase";
 import { ref, onValue, push, set } from "firebase/database";
 
 const pointsVariantId = import.meta.env.VITE_LEMONSQUEEZY_POINTS_VARIANT_ID;
--- src/firebase.ts
+++ src/firebase.ts
@@ -12,8 +12,8 @@ import {
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URLL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID
