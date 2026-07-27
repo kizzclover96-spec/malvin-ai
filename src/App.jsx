15d704ce2a638e8043f765e80ae3402dea538cbc
Praise Imasuen <kizzclover96@gmail.com>
2026-07-27 22:32:42 +0200

    WE

--- src/App.jsx
+++ src/App.jsx
@@ -248,14 +248,7 @@ function App() {
     if (type === "premium") { setFlowStep("premiumView"); return; }
   };
 
-  const params = new URLSearchParams(location.search);
-  const checkoutStatus = params.get("checkout");
-
-  if (checkoutStatus === "success" || checkoutStatus === "cancel") {
-    setFlowStep("front"); // straight to Customer Hub
-    navigate(location.pathname, { replace: true, state: {} }); // clean the URL
-    return;
-  }
+  
 
   return (
     <>
